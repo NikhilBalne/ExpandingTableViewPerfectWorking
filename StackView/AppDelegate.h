ec69fcb64f05f0835d64e1630314db401b366b28
Nikhil Balne <nikhilkumar.balne@gmail.com>
2019-11-16 23:16:45 +0530

    Initial Commit

---
+++ StackView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StackView
+//
+//  Created by ihub on 02/08/18.
+//  Copyright © 2018 ecoihub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StackView/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  StackView
+//
+//  Created by ihub on 02/08/18.
+//  Copyright © 2018 ecoihub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITableViewDelegate,UITableViewDataSource>
+
+@end
+
